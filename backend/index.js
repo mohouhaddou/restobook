f98f1dc0cb73ec510ec0ae061e1f93bd9dfebc90
lkrid <mouhamed.ouhaddou@gmail.com>
2025-09-17 17:02:52 +0100

    chore: initial commit (RestoBook frontend+backend)

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const path = require('path');
 const app = express();
 app.use(express.json());
